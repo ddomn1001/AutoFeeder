de0e7ef4d1e3e8f8d764e03061d1e5e40a5691e1
ddomn1001 <160621795+ddomn1001@users.noreply.github.com>
2024-03-17 22:26:49 +0000

    Update motor2.py

--- Backend/motor2.py
+++ Backend/motor2.py
@@ -3,8 +3,7 @@ from gpiozero import AngularServo
 import time
 
 #Time Constant, Written By Dominic Nguyen
-TIME_LIMIT_SECONDS = 10  #Time Desired
-
+TIME_LIMIT_SECONDS = 1.1
 #Written by Mason
 servo = AngularServo(18, min_pulse_width=0.0003, max_pulse_width=0.0026)
 
@@ -14,6 +13,8 @@ end_time = time.time() + TIME_LIMIT_SECONDS
 # Angle Written by Mason, While Loop modified and written by Dominic Nguyen
 while time.time() < end_time:
     servo.angle = -35
+   
 
 # After the loop, stop the servo - Written by Dominic Nguyen
 servo.angle = None
+time.sleep(1)
